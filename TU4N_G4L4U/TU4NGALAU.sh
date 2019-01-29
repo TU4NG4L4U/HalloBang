4c65d04987f0d0c8d09ee613d7566aca31a51c40
TU4NG4L4U <46773642+TU4NG4L4U@users.noreply.github.com>
2019-01-30 06:38:58 +0700

    Add files via upload

---
+++ TU4N_G4L4U/TU4NGALAU.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+#version 1.0
+
+#MAU NGAPAIN TOT?:V
+#RECODE?REEDIT?RECOPYRIGHT?COPAS?
+#MALU TOLOL!!!
+#KAGA BERMORAL!
+
+
+#01/08/18
+
+clear
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+figlet TUAN B4DUT  | lolcat
+
+echo -b 
+"_____________________________________________________________" 
+| lolcat
+echo -b "TYPE      : TOOLS INSTALLER $green " |lolcat
+echo -b "VERSION   : V.3 " | lolcat
+echo -b "TOTALS    : 311 TOOLS " | lolcat
+echo -b "AUTHOR    : TUAN B4DUT $green " |lolcat
+echo -b "ASSOCIATE : INDONESIAN TERMUX ASSOCIATION $green " 
+|lolcat
+echo -b "ASSOCIATE : INDONESIAN TERMUX TUTORIAL $green " 
+|lolcat
+echo -b "ASSOCIATE : INDONESIAN DARK TERMUX ASSOCIATE 
+$green " |lolcat
+echo -b "NOTES     : JELAJAHI SETIAP TOOLS DENGAN BIJAK " | 
+lolcat
+echo -b 
+"_____________________________________________________________" 
+| lolcat
+
+sleep 1
+
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo -b $green"[#]> TOOLS YANG KALIAN INSTALL ADA DI 
+DIRECTORY TOOLS GUA " |lolcat
+echo -b $green"[#]> Thanks For Using My Tools" |lolcat
+sleep 1
+echo -b $green"[#]> TUAN B4DUT On Your System" |lolcat
+
+echo -b $green"[#]> See you Again SCRIPTKIDDIES😎 :)..." 
+|lolcat
+figlet TUAN B4DUT | lolcat
+sleep 1
+exit
+}
+
+
+lagi=1
+while [ $lagi -lt 6 ];
+do
+
+echo ""
+echo ""
+
+echo -e "######################################" | lolcat
+echo -e "#SILAHKAN TUNGGU DAN LIHAT LIST TOOLS#" | lolcat
+echo -e "######################################" | lolcat
+
+echo ""
+echo -e "============================" | lolcat
+echo -e $b "1. Install Nmap${enda}";
+echo -e "============================" | lolcat
+echo -e $b "2. Install Admin-finder${endcla}";
+echo -e "============================" | lolcat
+echo -e $b "3. Install RED_HAWK${enda}";
+echo -e "============================" | lolcat
+echo -e $b "4. Install Lazymux${enda}";
+echo -e "============================" | lolcat
+echo -e $b "5. Install Tools-X${enda}";
+echo -e "============================" | lolcat
+echo -e $b "6. Install Mrcakil${enda}";
+echo -e "============================" | lolcat
+echo -e $b "7. Install D-TECT${enda}";
+echo -e "============================" | lolcat
+echo -e $b "8. Install Mr.Rv1.1${enda}";
+echo -e "============================" | lolcat
+echo -e $b "9. Install BAJINGANv6${enda}";
+echo -e "============================" | lolcat
+echo -e $b "10. Install MultiBruteForce(MBF)${enda}";
+echo -e "============================" | lolcat
+echo -e $b "11. Install XERXES${enda}";
+echo -e "============================" | lolcat
+echo -e $b "12. Install LITESPAM${enda}";
+echo -e "============================" | lolcat
+echo -e $b "13. Install Android-Malware${enda}";
+echo -e "============================" | lolcat
+echo -e $b "14. Install SigPloit${enda}";
+echo -e "============================" | lolcat
+echo -e $b "15. Install Sn1per${enda}";
+echo -e "============================" | lolcat
+echo -e $b "16. Install ICG-AutoExploiterBoT${enda}";
+echo -e "============================" | lolcat
+echo -e $b "17. Install QRLJacking${enda}";
+echo -e "============================" | lolcat
+echo -e $b "18. Install txtool${enda}";
+echo -e "============================" | lolcat
+echo -e $b "19. Install Bulltools${enda}";
+echo -e "============================" | lolcat
+echo -e $b "20. Install Termux-Banner${enda}";
+echo -e "============================" | lolcat
+echo -e $b "21. Install udfhack${enda}";
+echo -e "============================" | lolcat
+echo -e $b "22. Install Mirai-Source-Code${enda}";
+echo -e "============================" | lolcat
+echo -e $b "23. Install Hale${enda}";
+echo -e "============================" | lolcat
+echo -e $b "24. Install Mirai-IoT-BotNet${enda}";
+echo -e "============================" | lolcat
+echo -e $b "25. Install Ufonet${enda}";
+echo -e "============================" | lolcat
+read -p "[?] Silahkan masukkan pilhan 
+anda [•-•]:" pilih
+case $pilih in 
